376814ad05ea91967685bfe7f1314be6d1812a18
PatrikZh <patrik.zheji@gmail.com>
2022-12-23 07:55:14 -0800

    Number 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,12 +1,27 @@
 #include "sort.h"
 
 /**
- * bubble_sort - 
- * @array:
- * @size:
+ * bubble_sort - function
+ * @array: parameter
+ * @size: parameter
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	
+	int tmp;
+	size_t i, j;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
