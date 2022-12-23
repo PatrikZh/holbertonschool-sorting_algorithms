b63fe0393115fdb62b89ea6407526ee186f27e9f
PatrikZh <patrik.zheji@gmail.com>
2022-12-23 08:46:18 -0800

    Fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,5 +7,20 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	size_t i, j;
+	int tmp;
 
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
