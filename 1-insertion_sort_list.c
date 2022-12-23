3efaacfc635481a545513a4ac9cba424b4f37046
PatrikZh <patrik.zheji@gmail.com>
2022-12-23 08:21:07 -0800

    Changes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,11 +1,11 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list -
- * @list:
+ * insertion_sort_list - function
+ * @list: parameter
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	
+
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,25 @@
 #include "sort.h"
 
 /**
- * selection_sort -
- * @array:
- * @size:
+ * selection_sort - function
+ * @array: parameter
+ * @size: parameter
  */
 
 void selection_sort(int *array, size_t size)
 {
-	
+	size_t i, j;
+	int tmp;
+
+	for (j = 0; j < size; j++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			tmp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = tmp;
+			print_array(array, size);
+		}
+
+	}
 }
