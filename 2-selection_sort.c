3b19784cea218d9e97a9d676070604ab86763383
FabioH7 <fhysollari4@gmail.com>
2023-01-04 07:59:18 -0800

    adding selection sort algo

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,5 +8,27 @@
 
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, idx, flag = 0;
+	int tmp = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		tmp = array[i];
+		flag = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (tmp > array[j])
+			{
+				flag = 1;
+				tmp = array[j];
+				idx = j;
+			}
+		}
+		if (flag == 1)
+		{
+			array[idx] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
 }
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,52 +11,53 @@
 
 int partition(int *array, int first, int last, int size)
 {
-    int pivot = array[last];
-    int tmp;
-    int i = first - 1;
-    int j;
+	int pivot = array[last];
+	int tmp;
+	int i = first - 1;
+	int j;
 
-    for (j = first; j < size; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                tmp = array[j];
-                array[j] = array[i];
-                array[i] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
-    if (array[last] < array[i + 1])
-    {
-        tmp = array[last];
-        array[last] = array[i + 1];
-        array[i + 1] = tmp;
-        print_array(array, size);
-    }
-    return (i + 1);
+	for (j = first; j < size; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[j];
+				array[j] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[last] < array[i + 1])
+	{
+		tmp = array[last];
+		array[last] = array[i + 1];
+		array[i + 1] = tmp;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
 /**
  * lomuto - calls partition and parts the array in sorted and unsorted parts
  * @array: array to sort
  * @first: first index to look
+ * @size: array size
  * @last: last index to look
  */
 
 void lomuto(int *array, int first, int last, int size)
 {
-    int parted;
+	int parted;
 
-    if (first < last)
-    {
-        parted = partition(array, first, last, size);
-        lomuto(array, first, parted - 1, size);
-        lomuto(array, parted + 1, last, size);
-    }
+	if (first < last)
+	{
+		parted = partition(array, first, last, size);
+		lomuto(array, first, parted - 1, size);
+		lomuto(array, parted + 1, last, size);
+	}
 }
 
 /**
@@ -66,7 +67,7 @@ void lomuto(int *array, int first, int last, int size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-    lomuto(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	lomuto(array, 0, size - 1, size);
 }
